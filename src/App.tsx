281888f95931dd5a95676c120a64267547e964ff
Benício Barreto <benicio.daniel@gmail.com>
2020-09-22 20:41:53 -0300

    Creating Cart Context

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
+import { CartProvider } from './context/CartContext';
+
 import GlobalStyles from './styles';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes />
+      <CartProvider>
+        <Routes />
+      </CartProvider>
       <GlobalStyles />
     </BrowserRouter>
   );
---
+++ src/context/CartContext.tsx
@@ -0,0 +1,116 @@
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+} from 'react';
+import api from '../services/api';
+
+interface Product {
+  id: number;
+  nome: string;
+  valor_unitario: number;
+  quantidade: number;
+  url_imagem: string;
+  sku: string;
+  observacao?: string;
+}
+
+interface CartContextData {
+  products: Product[] | null;
+  incrementProductQuantity(id: number): void;
+  decrementProductQuantity(id: number): void;
+  removeProduct(id: number): void;
+  addNote(id: number, note: string): void;
+}
+
+const CartContext = createContext<CartContextData>({} as CartContextData);
+
+const CartProvider: React.FC = ({ children }) => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await api.get('/');
+      console.log(response.data);
+      setProducts(response.data);
+    }
+
+    loadData();
+  }, []);
+
+  const incrementProductQuantity = useCallback(
+    (id: number) => {
+      const newProducts = products?.map(product =>
+        product.id === id
+          ? { ...product, quantidade: product.quantidade + 1 }
+          : product,
+      );
+      setProducts(newProducts);
+    },
+    [products],
+  );
+
+  const decrementProductQuantity = useCallback(
+    (id: number) => {
+      const newProducts = products.map(product =>
+        product.id === id && product.quantidade > 0
+          ? { ...product, quantidade: product.quantidade - 1 }
+          : product,
+      );
+      setProducts(newProducts);
+    },
+    [products],
+  );
+
+  const removeProduct = useCallback(
+    (id: number) => {
+      const newProducts = products.filter(product => product.id !== id);
+      setProducts(newProducts);
+    },
+    [products],
+  );
+
+  const addNote = useCallback(
+    (id: number, note: string) => {
+      const newProducts = products.map(product =>
+        product.id === id ? { ...product, observacao: note } : product,
+      );
+      setProducts(newProducts);
+      console.log(newProducts, 'add note');
+    },
+    [products],
+  );
+
+  const value = React.useMemo(
+    () => ({
+      products,
+      incrementProductQuantity,
+      decrementProductQuantity,
+      removeProduct,
+      addNote,
+    }),
+    [
+      products,
+      incrementProductQuantity,
+      decrementProductQuantity,
+      removeProduct,
+      addNote,
+    ],
+  );
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+function useCart(): CartContextData {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error('You should use useCart within CartProvider');
+  }
+
+  return context;
+}
+
+export { CartProvider, useCart };
