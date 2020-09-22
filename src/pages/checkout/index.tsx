17921f333fe479e29b83c1df0cdb591eec00f77d
Benício Barreto <benicio.daniel@gmail.com>
2020-09-22 10:01:25 -0300

    Creating checkout Form page

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,6 +1,55 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useRef, useCallback } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
-const Checkout: React.FC = () => <Container>checkout page</Container>;
+import Header from '../../components/header';
+import Banner from '../../components/banner';
+
+import Input from '../../components/input';
+import Button from '../../components/button';
+
+import { PageContainer, Container } from './styles';
+
+interface FormData {
+  enderecoRua: string;
+  enderecoNumero: number;
+  enderecoBairro: string;
+  cartaoNumero: number;
+  cartaoCVC: number;
+}
+
+const Checkout: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback((formData: FormData) => {
+    console.log(formData);
+  }, []);
+
+  return (
+    <PageContainer>
+      <Header />
+      <Banner />
+      <Container>
+        <h1>Finalizar Compra</h1>
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Input name="enderecoRua" placeholder="Digite o seu endereço" />
+          <div>
+            <Input name="enderecoNumero" placeholder="Número" />
+            <Input name="enderecoBairro" placeholder="Bairro" />
+          </div>
+          <span>
+            <Input
+              name="cartaoNumero"
+              placeholder="Digite os números do seu cartão"
+            />
+            <Input name="cartaoCVC" placeholder="CVC" />
+          </span>
+          <Button type="submit">Finalizar</Button>
+        </Form>
+      </Container>
+    </PageContainer>
+  );
+};
 
 export default Checkout;
--- src/pages/checkout/styles.ts
+++ src/pages/checkout/styles.ts
@@ -1,3 +1,61 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const PageContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  /* max-width: 1200px; */
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  padding: 30px 50px 40px 50px;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 32px;
+    line-height: 40px;
+    letter-spacing: -1.5px;
+
+    margin-bottom: 30px;
+  }
+
+  form {
+    min-width: 350px;
+
+    > div + div {
+      margin-top: 15px;
+    }
+
+    div {
+      display: flex;
+
+      div:first-child {
+        width: 33%;
+        margin-right: 6px;
+      }
+    }
+
+    span {
+      margin-top: 15px;
+      display: flex;
+
+      div:first-child {
+        width: 67%;
+        margin-right: 6px;
+      }
+
+      div:last-child {
+        width: 33%;
+      }
+    }
+  }
+`;
