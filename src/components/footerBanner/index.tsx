45bf0b0db419e8e6edcb249712efadba24238f1a
Benício Barreto <benicio.daniel@gmail.com>
2020-09-24 16:53:46 -0300

    Added footer banner

---
+++ src/components/footerBanner/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FooterBanner as FootBanner } from './styles';
+
+const FooterBanner: React.FC = () => {
+  return <FootBanner />;
+};
+
+export default FooterBanner;
---
+++ src/components/footerBanner/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FooterBanner = styled.div`
+  height: 40px;
+  width: 100vw;
+  /* margin-top: 40px; */
+  position: absolute;
+  bottom: 0;
+  overflow: hidden;
+  background: #f5f5f5;
+`;
